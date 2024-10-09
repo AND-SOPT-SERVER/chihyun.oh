e3e9809842d6e01b65e8ec67cec208b60de78802
chyun <och2510@gmail.com>
2024-10-09 22:38:19 +0900

    feat: 일기 리스트 조회 기능 추가

--- src/main/java/org/sopt/week1/DiaryService.java
+++ src/main/java/org/sopt/week1/DiaryService.java
@@ -1,13 +1,19 @@
 package org.sopt.week1;
 
+import java.util.List;
+
 import org.sopt.week1.DiaryRepository;
 
 public class DiaryService {
 	private final DiaryRepository diaryRepository = new DiaryRepository();
 
-	public void writeDiary(final String body) {
+	void writeDiary(final String body) {
 		Diary diary = new Diary(null, body.trim());
 
 		diaryRepository.save(diary);
+	}
+
+	List<Diary> getDiaryList() {
+		return diaryRepository.findAll();
 	}
 }
