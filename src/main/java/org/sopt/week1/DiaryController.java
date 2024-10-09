49e70ac4bd251e0b37525854b2067e5340ba52e4
chyun <och2510@gmail.com>
2024-10-09 23:32:09 +0900

    feat: 삭제된 일기 영구 삭제 기능 추가

--- src/main/java/org/sopt/week1/DiaryController.java
+++ src/main/java/org/sopt/week1/DiaryController.java
@@ -63,6 +63,12 @@ public class DiaryController {
 		return diaryService.getRecoveryDiaryList();
 	}
 
+	final void recoveryDelete(final String id) {
+		validateIdType(id);
+
+		diaryService.recoveryDeleteDiary(Long.parseLong(id));
+	}
+
 	final void recoveryPatch(final String id) {
 		validateIdType(id);
 
