d6b0ea41b07c2f27518fe8adf7b9db590d6e89d0
chyun <och2510@gmail.com>
2024-10-09 23:38:54 +0900

    style: 이름 변경

--- src/main/java/org/sopt/week1/Main.java
+++ src/main/java/org/sopt/week1/Main.java
@@ -1,7 +1,5 @@
 package org.sopt.week1;
 
-import org.sopt.week1.DiaryController;
-
 import java.io.*;
 
 public class Main {
@@ -94,13 +92,13 @@ public class Main {
 
 							server.patch(inputId, inputBody);
 						}
-						case "RECOVERY" -> {
+						case "RESTORE" -> {
 							ConsoleIO.printLine("어떤 작업을 할건지 선택해주세요!");
 							final String selected = ConsoleIO.readLine();
 
 							switch (selected) {
 								case "GET" -> {
-									server.recoveryGetList().forEach(diary -> {
+									server.restoreGetList().forEach(diary -> {
 										try {
 											ConsoleIO.printLine(diary.getId() + " : " + diary.getBody());
 										} catch (IOException e) {
@@ -112,13 +110,13 @@ public class Main {
 									ConsoleIO.printLine("영구 삭제할 id 를 입력하세요!");
 									final String inputId = ConsoleIO.readLine();
 
-									server.recoveryDelete(inputId);
+									server.restoreDelete(inputId);
 								}
 								case "PATCH" -> {
 									ConsoleIO.printLine("복구할 id 를 입력하세요!");
 									final String inputId = ConsoleIO.readLine();
 
-									server.recoveryPatch(inputId);
+									server.restorePatch(inputId);
 								}
 							}
 						}
@@ -153,7 +151,7 @@ public class Main {
                     - POST : 일기 작성하기
                     - DELETE : 일기 제거하기
                     - PATCH : 일기 수정하기
-                    - RECOVERY
+                    - RESTORE
                     	- GET : 삭제된 일기 불러오기
                     	- PATCH : 삭제된 일기 복구하기
                     	- DELETE : 삭제된 일기 영구 삭제하기
