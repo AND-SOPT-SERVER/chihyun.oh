c91e851c68bbe8015a780f5d17856b08c522d14e
chyun <och2510@gmail.com>
2024-10-09 22:33:30 +0900

    feat: 일기 작성 기능 추가

--- src/main/java/org/sopt/week1/DiaryRepository.java
+++ src/main/java/org/sopt/week1/DiaryRepository.java
@@ -8,5 +8,13 @@ public class DiaryRepository {
 	private final Map<Long, String> storage = new ConcurrentHashMap<>();
 	private final AtomicLong numbering = new AtomicLong();
 
+	public void save(final Diary diary) {
+		if (diary.getId() != null) {
+			storage.put(diary.getId(), diary.getBody());
+			return;
+		}
 
+		final Long id = numbering.getAndAdd(1);
+		storage.put(id, diary.getBody());
+	}
 }
