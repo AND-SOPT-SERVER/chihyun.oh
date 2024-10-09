4d48bc23cac866b11b2af2ab689d2ccfdacc33ac
chyun <och2510@gmail.com>
2024-10-09 22:33:30 +0900

    feat: 일기 작성 기능 추가

--- src/main/java/org/sopt/week1/DiaryService.java
+++ src/main/java/org/sopt/week1/DiaryService.java
@@ -4,4 +4,10 @@ import org.sopt.week1.DiaryRepository;
 
 public class DiaryService {
 	private final DiaryRepository diaryRepository = new DiaryRepository();
+
+	public void writeDiary(final String body) {
+		Diary diary = new Diary(null, body.trim());
+
+		diaryRepository.save(diary);
+	}
 }
