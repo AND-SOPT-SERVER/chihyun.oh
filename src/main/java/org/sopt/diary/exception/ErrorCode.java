45ae5db0e554c5d27e91e3039fb62d495e3bc526
chyun <och2510@gmail.com>
2024-10-23 20:35:59 +0900

    feat: 일기 제목 중복 금지 기능 추가

--- src/main/java/org/sopt/diary/exception/ErrorCode.java
+++ src/main/java/org/sopt/diary/exception/ErrorCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     TOO_MANY_PATCH_REQUEST(HttpStatus.TOO_MANY_REQUESTS, "조금 뒤에 다시 시도해주세요."),
     DIARY_NOT_EXIST(HttpStatus.BAD_REQUEST, "존재하지 않는 일기에요."),
-    CONTENT_LENGTH_OVER(HttpStatus.BAD_REQUEST, "일기 내용은 30자까지 가능해요.");
+    CONTENT_LENGTH_OVER(HttpStatus.BAD_REQUEST, "일기 내용은 30자까지 가능해요."),
+    TITLE_DUPLICATED(HttpStatus.BAD_REQUEST, "이미 존재하는 제목이에요.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/org/sopt/diary/repository/DiaryRepository.java
+++ src/main/java/org/sopt/diary/repository/DiaryRepository.java
@@ -1,10 +1,13 @@
 package org.sopt.diary.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface DiaryRepository extends JpaRepository<DiaryEntity, Long> {
     public List<DiaryEntity> findTop10ByOrderByCreatedAtDesc();
+
+    public Optional<DiaryEntity> findByTitle(String title);
 }
