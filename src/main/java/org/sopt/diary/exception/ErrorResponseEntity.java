3f7c98cd930699f19b20d66fbe40a1a0a00676dc
chyun <och2510@gmail.com>
2024-10-23 20:19:31 +0900

    feat: 일기 수정 후 수정 5분 제한 기능 추가

---
+++ src/main/java/org/sopt/diary/exception/ErrorResponseEntity.java
@@ -0,0 +1,27 @@
+package org.sopt.diary.exception;
+
+import org.springframework.http.ResponseEntity;
+
+public class ErrorResponseEntity {
+    private int status;
+    private String name;
+    private String message;
+
+    private ErrorResponseEntity(int status, String name, String message) {
+        this.status = status;
+        this.name = name;
+        this.message = message;
+    }
+
+    public static ResponseEntity<ErrorResponseEntity> toResponseEntity(ErrorCode e) {
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(
+                        new ErrorResponseEntity(
+                                e.getHttpStatus().value(),
+                                e.name(),
+                                e.getMessage()
+                        )
+                );
+    }
+}
--- src/main/java/org/sopt/diary/service/DiaryService.java
+++ src/main/java/org/sopt/diary/service/DiaryService.java
@@ -1,9 +1,12 @@
 package org.sopt.diary.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.sopt.diary.constant.ErrorMessage;
 import org.sopt.diary.dto.Diary;
+import org.sopt.diary.exception.CustomException;
+import org.sopt.diary.exception.ErrorCode;
 import org.sopt.diary.repository.DiaryEntity;
 import org.sopt.diary.repository.DiaryRepository;
 import org.springframework.stereotype.Component;
@@ -11,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class DiaryService {
+    private static final int PATCH_TIME_LIMIT = 5;
     private final DiaryRepository diaryRepository;
 
     public DiaryService(DiaryRepository diaryRepository) {
@@ -20,6 +24,18 @@ public class DiaryService {
     private void validateExist(Optional<DiaryEntity> diaryEntity) {
         if (diaryEntity.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.NOT_EXIST_DIARY.getMessage());
+        }
+    }
+
+    private void validateOverEnablePatchTime(DiaryEntity diaryEntity) {
+        LocalDateTime enablePatchTime =
+                LocalDateTime.from(
+                        diaryEntity.getUpdatedAt()
+                ).plusMinutes(PATCH_TIME_LIMIT);
+
+        if (diaryEntity.getCreatedAt().isBefore(diaryEntity.getUpdatedAt()) &&
+                LocalDateTime.now().isBefore(enablePatchTime)) {
+            throw new CustomException(ErrorCode.TOO_MANY_PATCH_REQUEST);
         }
     }
 
@@ -46,10 +62,11 @@ public class DiaryService {
     @Transactional
     public void update(final Long id, final Diary diary) {
         Optional<DiaryEntity> foundDiaryEntity = diaryRepository.findById(id);
-
         validateExist(foundDiaryEntity);
 
         DiaryEntity diaryEntity = foundDiaryEntity.get();
+        validateOverEnablePatchTime(diaryEntity);
+
         diaryEntity.setTitle(diary.getTitle());
         diaryEntity.setContent(diary.getContent());
 
