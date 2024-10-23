80d76aefe657a03b69c17f5518f6ecc4e1f0f1bd
chyun <och2510@gmail.com>
2024-10-24 01:06:29 +0900

    refactor: 일기 목록 조회 API 명세 변경

--- src/main/java/org/sopt/diary/dto/response/DiaryResponse.java
+++ src/main/java/org/sopt/diary/dto/response/DiaryResponse.java
@@ -6,21 +6,18 @@ public class DiaryResponse {
     private final long id;
     private final String category;
     private final String title;
-    private final String content;
 
-    private DiaryResponse(long id, String category, String title, String content) {
+    private DiaryResponse(long id, String category, String title) {
         this.id = id;
         this.category = category;
         this.title = title;
-        this.content = content;
     }
 
     public static DiaryResponse toDiaryResponse(final Diary diary) {
         return new DiaryResponse(
                 diary.getId(),
                 diary.getCategory(),
-                diary.getTitle(),
-                diary.getContent()
+                diary.getTitle()
         );
     }
 
@@ -34,9 +31,5 @@ public class DiaryResponse {
 
     public String getTitle() {
         return title;
-    }
-
-    public String getContent() {
-        return content;
     }
 }
