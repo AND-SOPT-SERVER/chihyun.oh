94873200f42ae82b996b88e3868aa874e6672d85
chyun <och2510@gmail.com>
2024-10-26 23:41:03 +0900

    refactor: 응답 본문 삭제

--- src/main/java/org/sopt/diary/constant/ResponseMessage.java
+++
@@ -1,7 +0,0 @@
-package org.sopt.diary.constant;
-
-public class ResponseMessage {
-    public static final String DIARY_CREATED = "일기가 생성되었습니다.";
-    public static final String DIARY_UPDATED = "일기가 수정되었습니다.";
-    public static final String DIARY_DELETED = "일기가 삭제되었습니다.";
-}
--- src/main/java/org/sopt/diary/exception/ErrorCode.java
+++ src/main/java/org/sopt/diary/exception/ErrorCode.java
@@ -3,24 +3,18 @@ package org.sopt.diary.exception;
 import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
-    TOO_MANY_PATCH_REQUEST(HttpStatus.TOO_MANY_REQUESTS, "조금 뒤에 다시 시도해주세요."),
-    DIARY_NOT_EXIST(HttpStatus.BAD_REQUEST, "존재하지 않는 일기에요."),
-    CONTENT_LENGTH_OVER(HttpStatus.BAD_REQUEST, "일기 내용은 30자까지 가능해요."),
-    TITLE_DUPLICATED(HttpStatus.BAD_REQUEST, "이미 존재하는 제목이에요.");
+    TOO_MANY_PATCH_REQUEST(HttpStatus.TOO_MANY_REQUESTS),
+    DIARY_NOT_EXIST(HttpStatus.BAD_REQUEST),
+    CONTENT_LENGTH_OVER(HttpStatus.BAD_REQUEST),
+    TITLE_DUPLICATED(HttpStatus.BAD_REQUEST);
 
     private final HttpStatus httpStatus;
-    private final String message;
 
-    ErrorCode(HttpStatus httpStatus, String message) {
+    ErrorCode(HttpStatus httpStatus) {
         this.httpStatus = httpStatus;
-        this.message = message;
     }
 
     public HttpStatus getHttpStatus() {
         return httpStatus;
-    }
-
-    public String getMessage() {
-        return message;
     }
 }
