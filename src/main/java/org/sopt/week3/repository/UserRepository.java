4aee382c9cac25c8e32f3876b978e9b68467dba8
chyun <och2510@gmail.com>
2024-10-31 14:29:11 +0900

    feat: 유저 엔티티와 레포지토리 추가

--- src/main/java/org/sopt/week3/repository/UserRepository.java
+++ src/main/java/org/sopt/week3/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package org.sopt.week3.repository;
 
-import org.sopt.week3.entity.SoptMemberEntity;
+import org.sopt.week3.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SoptMemberRepository extends JpaRepository<SoptMemberEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
