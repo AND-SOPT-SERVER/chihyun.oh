1679aa21fb5bc8b5bd8ff4435784aaf4e8459586
chyun <och2510@gmail.com>
2024-10-31 19:03:21 +0900

    feat: 일기 작성 기능 추가

--- src/main/java/org/sopt/week3/constant/Category.java
+++ src/main/java/org/sopt/week3/constant/Category.java
@@ -1,5 +1,7 @@
 package org.sopt.week3.constant;
 
+import java.util.Arrays;
+
 public enum Category {
     FOOD("음식"),
     SCHOOL("학교"),
@@ -10,6 +12,16 @@ public enum Category {
 
     Category(String name) {
         this.name = name;
+    }
+
+    public static Category getCategoryByName(final String name) {
+        return Arrays.stream(values())
+                .filter(category -> category.name.equals(name))
+                .findFirst()
+                .orElseThrow(
+                        // 에러 추가
+                        () -> new IllegalArgumentException()
+                );
     }
 
     public String getName() {
