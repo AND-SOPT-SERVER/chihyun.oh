661dd4bdedce9060c8f5caf6a7afbbf30685f8db
chyun <och2510@gmail.com>
2024-10-31 18:45:55 +0900

    feat: 내 일기 조회 목록 헤더에서 id 값 가져오도록 변경

--- src/main/java/org/sopt/week3/controller/DiaryController.java
+++ src/main/java/org/sopt/week3/controller/DiaryController.java
@@ -2,14 +2,12 @@ package org.sopt.week3.controller;
 
 import java.util.List;
 import org.sopt.week3.dto.diary.DiaryDTO;
-import org.sopt.week3.dto.diary.request.MyDiariesRequest;
 import org.sopt.week3.dto.diary.response.DiariesResponse;
 import org.sopt.week3.dto.diary.response.MyDiariesResponse;
-import org.sopt.week3.dto.user.UserDTO;
 import org.sopt.week3.service.DiaryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,11 +36,12 @@ public class DiaryController {
     ResponseEntity<MyDiariesResponse> getMyDiaries(
             @RequestParam(required = false, defaultValue = "createdAt", value = "orderby") final String criteria,
             @RequestParam(required = false, defaultValue = "0") int page,
-            @RequestBody MyDiariesRequest myDiariesRequest
+            @RequestHeader long id
     ) {
-        List<DiaryDTO> diaryDTOs = diaryService.getMyDiaries(UserDTO.toUserDTO(myDiariesRequest), criteria, page);
+        // 헤더로 유저 아이디 검사 추가
+        List<DiaryDTO> diaryDTOs = diaryService.getMyDiaries(id, criteria, page);
         MyDiariesResponse myDiariesResponse = MyDiariesResponse.toMyDiariesResponse(diaryDTOs);
-        
+
         return ResponseEntity.ok(myDiariesResponse);
     }
 }
--- src/main/java/org/sopt/week3/dto/diary/request/MyDiariesRequest.java
+++
@@ -1,6 +0,0 @@
-package org.sopt.week3.dto.diary.request;
-
-public record MyDiariesRequest(
-        long id
-) {
-}
